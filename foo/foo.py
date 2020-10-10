f9a29de76efd93f9bf87a41954469a3ad9e43473
Matt Hamilton <matthew.hamilton1@ibm.com>
2020-10-10 10:08:14 +0100

    add another function

--- foo/foo.py
+++ foo/foo.py
@@ -1,6 +1,11 @@
 def afunction(x):
+    """A simple function"""
     if x > 5:
         return x + 1
     else:
         return x
+
+def another_function(x):
+    """Another function"""
+    return x * 2
 
